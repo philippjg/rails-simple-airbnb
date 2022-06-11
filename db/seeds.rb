a0d8cf1a26784d27fcb17232deb3228a0b5a9ab7
philipp galbo <pjgalbo@gmail.com>
2022-06-11 11:08:07 +1000

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Garden Shed',
+  address: 'Sydney',
+  description: 'Great to live with some tools',
+  price_per_night: 200,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Small Tent',
+  address: 'Melbourne',
+  description: 'Get in touch with the outdoors',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Outhouse',
+  address: 'Melbourne',
+  description: 'Cant get any better than this',
+  price_per_night: 10,
+  number_of_guests: 1
+)
